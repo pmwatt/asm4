0858eee28b471eef206d14beb4fdc4f690d82d96
pmwatt <prachnachai.mek@student.mahidol.edu>
2024-04-25 21:41:35 +0700

    add instructions

--- asm4/asm4.cpp
+++ asm4/asm4.cpp
@@ -6,24 +6,52 @@
 //!
 //! Author: Mores Prachyabrued.
 //!
-//! Keyboard inputs for plane and propeller (subpart):
-//!   s   = moves the plane forward
-//!   f   = moves the plane backward
-//!   q,e = rolls the plane
-//!   a   = yaws the plane
-//!   x   = pitches the plane
-//!
-//!   r   = rotates propeller
-//!
-//!   i,k = translates light up/down
-//!   9   = toggles diffuse light on/off
-//!
-//! Mouse inputs for world-relative camera:
-//!   Hold left button and drag  = controls azimuth and elevation 
-//!                                (Press CTRL (and hold) before left button to restrict to azimuth control only,
-//!                                 Press SHIFT (and hold) before left button to restrict to elevation control only)   
-//!   Hold right button and drag = controls distance
-//!
+//! 
+
+/////////////////////////////////////////////////
+//# ITCS481 Assignment 4
+//
+//## Member
+//6488130 Warittha Tarntong 
+//6488160 Prachnachai Meakpaiboonwattana 
+//6488221 Thai Mekratanavorakul
+//
+//This assignment is an extension of from the previous assignment to improve visual quality. In this assignment, we have implemented OpenGL lighting, texturing, a skybox, and additional objects.
+//
+//
+//Keyboard inputs for camera, plane (turtle), and subparts (wings and cannon):
+//Camera:
+//Select camera to control: b
+//Select camera to view: v
+//
+//Plane (Turtle):
+//s = moves the plane forward
+//f = moves the plane backward
+//e = rolls the plane (+Z rot)
+//q = rolls the plane (-Z rot)
+//a = yaws the plane (+Y rot)
+//d = yaws the plane (-Y rot)
+//z = pitches the plane (+X rot)
+//c = pitches the plane (-X rot)
+//
+//Lighting:
+//i = Light up (+Y translation)
+//k = Light down (-Y translation)
+//o = Light right (+X translation)
+//u = Light left (-X translation)
+//l = Light forward (+Z translation)
+//j = Light backward (-Z translation)
+//
+//9 = Toggles diffuse light ON/OFF
+//8 = Toggles ambient light ON/OFF
+//0 = Toggles specular light ON/OFF
+//
+//## Additional
+//Non-trivial object = turtle cannon
+//Skybox reference = https://jkhub.org/files/file/3216-underwater-skybox/
+//Texturing besides skybox = cube rock, seaweed, sandfloor
+//////////////////////////
+
 //! TODO: Extend the code to satisfy the requirements given in the assignment handout
 //!
 //! Note: Good programmer uses good comments! :)
